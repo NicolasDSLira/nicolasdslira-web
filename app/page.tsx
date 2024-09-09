c669e5ba7c0510ce4b4b4724864891c5b2cfa602
Nicolas dos Santos Lira <80727008+NicolasDSLira@users.noreply.github.com>
2024-09-08 21:09:11 -0300

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,10 @@ import { LetterFx } from "./components/LetterFx";
 import Link from "next/link";
 import MatrixEffect from "./components/MatrixEffect";
 
+
+
 export default function Home() {
+  
   return (
     <>
       <div className="self-center flex flex-col gap-10 mx-20">
@@ -23,12 +26,12 @@ export default function Home() {
 
       </div>
 
-      <div style={styles.container}>
-        <div style={styles.monitor}>
-          <div style={styles.screen}>
+      <div className="container">
+        <div className="monitor">
+          <div className="screen">
             <MatrixEffect />
           </div>
-          <div style={styles.stand}></div>
+          <div className="stand"></div>
         </div>
       </div>
 
@@ -38,37 +41,3 @@ export default function Home() {
   );
 }
 
-const styles = {
-  container: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: '40%',
-    height: 'calc(100dvh-7rem)',
-  },
-  monitor: {
-    width: '70%', 
-    height: '50%', 
-    backgroundColor: '#000',
-    borderRadius: '20px',
-    position: 'relative',
-    boxShadow: '0 0 20px rgba(0, 0, 0, 0.5)',
-  },
-  screen: {
-    width: '95%',
-    height: '80%',
-    backgroundColor: '#111', 
-    margin: '2.5% auto 0',
-    borderRadius: '10px',
-    overflow: 'hidden', 
-  },
-  stand: {
-    width: '10%',
-    height: '10%',
-    backgroundColor: '#666',
-    position: 'absolute',
-    bottom: '-10%',
-    left: '45%',
-    borderRadius: '5px',
-  },
-};
